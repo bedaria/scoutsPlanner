3c6466e58dca21cda9dea6bcd28dad52ed3331e7
bedaria <dburinsk@gmail.com>
2017-03-29 19:56:14 -0700

    [setup] - sets up express server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.set('port', 3000)
+
+app.listen(app.get('port'), () =>
+  console.log('Express server listening on port: ', app.get('port'))
+)
