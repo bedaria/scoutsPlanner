c01312a12b7f59dde03782d3e7337cd2531d2671
bedaria <dburinsk@gmail.com>
2017-03-30 00:57:12 -0700

    [fix] - adds error detection if .env file is not found

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,22 @@
 'use strict'
 
-require('dotenv').config()
+const dotenv = require('dotenv').config()
 
-const environmentVariables = ['DB_URI']
-environmentVariables.forEach(name => {
-  if(!process.env[name])
-    throw new Error(`Environment variable ${name} is missing`)
-})
+if(dotenv.error)
+  throw new Error(`Can't find .env file in root path: ${dotenv.error}`)
+else {
+  const environmentVariables = ['DB_URI']
+  environmentVariables.forEach(name => {
+    if(!process.env[name])
+      throw new Error(`Environment variable ${name} is missing`)
+  })
 
-const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3000
 
-const config = {
-  db_uri: process.env.DB_URI,
-  port: port
+  const config = {
+    db_uri: process.env.DB_URI,
+    port: port
+  }
+
+  module.exports = config
 }
-
-module.exports = config
