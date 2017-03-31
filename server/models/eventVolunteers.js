d5f0b49549c4b1a13b221e98b324231d11ab9bae
bedaria <dburinsk@gmail.com>
2017-03-30 19:43:45 -0700

    [setup] - adds columns 'attending' and 'see' to table 'EventVolunteers'

--- server/models/eventVolunteers.js
+++ server/models/eventVolunteers.js
@@ -3,7 +3,9 @@
 module.exports = function(DB, DataTypes) {
   var EventVolunteers = DB.define('EventVolunteers', {
     startTime: DataTypes.STRING,
-    endTime: DataTypes.STRING
+    endTime: DataTypes.STRING,
+    attending: DataTypes.BOOLEAN,
+    seen: DataTypes.BOOLEAN
   })
   return EventVolunteers
 }
