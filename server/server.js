aa81e0364b445f977980a69da01f40024d89d65f
bedaria <dburinsk@gmail.com>
2017-03-31 22:48:11 -0700

    [feature/event] - adds routing for events

--- server/server.js
+++ server/server.js
@@ -5,12 +5,17 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const port = require('./config/config.js').port
 const db = require('./models/index.js')
+const event = require('./routes/event.js')
+const authentication = require('./middleware/authentication.js')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.use('/users/:name', authentication.isAuthenticated, event)
+app.use('/admins/:name', authentication.isAdmin, event)
 
 app.set('port', port)
 
