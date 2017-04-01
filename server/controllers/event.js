c8e69aaf6ebd0b77687222d5dfcee2e93b8eef38
bedaria <dburinsk@gmail.com>
2017-03-31 23:35:02 -0700

    [fix] - takes out unnecessary if/else statement

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -3,31 +3,28 @@
 const models = require('../models/index.js')
 
 const create = function(req, res){
+  //to create an Event row
   //Required: <string> name
   //Optional: <string> startTime,
   //          <string> endTime
 
   //req will have a req.user.name property from middleware
-  if(req.body.name && req.user.name){
-    const createEvent = models.Event.create(req.body)
-    const findUser = models.User.findOne({where: {name: req.user.name}})
+  const createEvent = models.Event.create(req.body)
+  const findUser = models.User.findOne({where: {name: req.user.name}})
 
-    Promise.all([createEvent, findUser])
-      .then(results => {
-        const event = results[0]
-        const user = results[1]
+  Promise.all([createEvent, findUser])
+    .then(results => {
+      const event = results[0]
+      const user = results[1]
 
-        if(user)
-          return event.setMainAdmin(user)
-        else
-          throw new Error(`Could not find user ${user}`)
-      })
-      .then(event =>
-        res.json({dataValues: event.dataValues}).status(200).end())
-      .catch(err => res.status(500).end())
-  }
-  else
-    res.status(400).end()
+      if(user)
+        return event.setMainAdmin(user)
+      else
+        throw new Error(`Could not find user ${user}`)
+    })
+    .then(event =>
+      res.json({dataValues: event.dataValues}).status(200).end())
+    .catch(err => res.status(500).end())
 }
 
 exports.event = {
