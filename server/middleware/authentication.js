fddc97b18201b289041da55b43396dac965e3271
bedaria <dburinsk@gmail.com>
2017-04-06 18:10:32 -0700

    [setup] - adds user-to-id table for fake authentication

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,14 +1,20 @@
+const users = {
+  'Frankie': 1,
+  'Copper': 2,
+  'Aidan': 3
+}
+
 const isAuthenticated =  function (req, res, next) {
   req.user = {}
   req.user.name = req.params.name
-  req.user.id = 3
+  req.user.id = users[req.user.name]
   next()
 }
 
 const isAdmin = function(req, res, next) {
   req.user = {}
   req.user.name = req.params.name
-  req.user.id = 3
+  req.user.id = users[req.user.name]
   next()
 }
 
