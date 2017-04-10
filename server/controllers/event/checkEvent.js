74d79c75300c81390391642de893519a462558ab
bedaria <dburinsk@gmail.com>
2017-04-09 17:35:29 -0700

    Finishes filtering data in checkEvent

--- server/controllers/event/checkEvent.js
+++ server/controllers/event/checkEvent.js
@@ -2,12 +2,17 @@
 
 const models = require('../../models/index.js')
 
-//Gets updated info on event for an admin
+//Gets info on event for an admin
 //resp.body will have:
-//        <array> {userInfo: models.Event.dataValues,
-//                 attendanceInfo: models.EventVolunteer.dataValues,
-//                 attendance: {totalAttending: <int>, totalMaybe: <int>,
-//                 totalNo: <int>, totalNotChecked}:<int>}
+//    <array<{volunteers: {name: <string>,
+//                         id: <string>,
+//                         isAttending: <string>
+//                         startTime: <string>,
+//                         endTime: <string>},
+//            attendance: {no: {names:<array>, count: <number>},
+//                        yes: {names:<array>, count: <number>},
+//                        maybe: {names:<array>, count: <number>},
+//                        noAnswer: {names:<array>, count: <number>}}}>>
 const checkEvent = function(req, res) {
   if(!req.user)
     Promise.reject("User must be logged in.")
@@ -18,33 +23,18 @@ const checkEvent = function(req, res) {
     models.Event.findOne({where: {id: req.params.event}})
       .then(event => {
         const isAdminOfEvent = event.dataValues.mainAdminId === req.user.id
+
         if(isAdminOfEvent)
           return event.getVolunteer()
         else
-          throw new Error(`Not an admin!`)
+          return Promise.reject("Not an admin!")
       })
-      .then(users => {
-        var attendance = {}
-        attendance.totalAttending = 0
-        attendance.totalMaybe = 0
-        attendance.totalNo = 0
-        attendance.totalNotChecked = 0
+      .then(volunteers => {
+        volunteers = filterVolunteerInfo(volunteers)
+        const attendance = getAttendanceInfo(volunteers)
+        const timeBlocks = getTimeBlocks(volunteers)
 
-        if(users)
-          users = users.map(user => {
-            var userAttendance = user.dataValues.EventVolunteer.isAttending
-
-            switch (userAttendance) {
-              case 'Yes': attendance.totalAttending++
-              case 'No': attendance.totalNo++
-              case 'Maybe': attendance.totalMaybe++
-              default: attendance.totalNotChecked++
-            }
-
-            return {userInfo: user.dataValues, attendanceInfo: user.dataValues.EventVolunteer}
-          })
-
-        res.json({users: users, attendance: attendance}).status(200).end()
+        res.json({volunteers, attendance, timeBlocks}).status(200).end()
       })
       .catch(err => {
         console.log(__filename, ": ERROR: ", err)
@@ -52,5 +42,144 @@ const checkEvent = function(req, res) {
       })
   }
 }
+
+//INPUT: volunteers: <array<models.User>>
+//OUTPUT: volunteers: {name: <string>,
+//                     id: <string>,
+//                     isAttending: <string>
+//                     startTime: <string>,
+//                     endTime: <string>}
+const filterVolunteerInfo = (volunteers) => {
+  return volunteers.map(volunteer => {
+    return {
+      name: volunteer.dataValues.name,
+      id: volunteer.dataValues.id,
+      startTime: volunteer.dataValues.EventVolunteer.startTime,
+      endTime: volunteer.dataValues.EventVolunteer.endTime,
+      isAttending: volunteer.dataValues.EventVolunteer.isAttending
+    }
+  })
+}
+
+//INPUT: volunteers: {name: <string>,
+//                    id: <string>,
+//                    isAttending: <string>
+//                    startTime: <string>,
+//                    endTime: <string>}
+//OUTPUT: attendance: {no: {names:<array>, count: <number>},
+//                     yes: {names:<array>, count: <number>},
+//                     maybe: {names:<array>, count: <number>},
+//                     noAnswer: {names:<array>, count: <number>}}}>>
+const getAttendanceInfo = (volunteers) => {
+  var attending = {count: 0, names: []}
+  var maybe = {count: 0, names: []}
+  var no = {count: 0, names: []}
+  var noAnswer = {count: 0, names: []}
+
+  volunteers.forEach(volunteer => {
+    var isAttending = volunteer.isAttending
+    var name = volunteer.name
+    var id = volunteer.id
+    const addTo = (obj) => {
+      obj.count++
+      obj.names.push({name, id})
+    }
+
+    switch (volunteer.isAttending) {
+      case 'Yes':
+        addTo(attending)
+        break
+      case 'No':
+        addTo(no)
+        break
+      case 'Maybe':
+        addTo(maybe)
+        break
+      default:
+        addTo(noAnswer)
+    }
+  })
+
+  return {attending, maybe, no, noAnswer}
+}
+
+//INPUT: volunteers: {name: <string>,
+//                    id: <string>,
+//                    isAttending: <string>
+//                    startTime: <string>,
+//                    endTime: <string>}
+//OUTPUT: uniqueTimesL <array<string>> (string: "12:00" or "17:15")
+const getUniqueTimes = (volunteers) => {
+  var timesSet = new Set()
+
+  volunteers.forEach(volunteer => {
+    var startTime = volunteer.startTime
+    var endTime = volunteer.endTime
+
+    if(startTime && endTime) {
+      timesSet.add(startTime)
+      timesSet.add(endTime)
+    }
+  })
+
+  return Array.from(timesSet).sort()
+}
+
+//INPUT: n: <number>
+//OUTPUT: [0,0,0.....,0]
+const toZeroArray = (n) => {
+  var array = []
+  for(var i = 0; i < n; i++)
+    array[i] = 0
+  return array
+}
+
+//INPUT: volunteers: {name: <string>,
+//                    id: <string>,
+//                    isAttending: <string>
+//                    startTime: <string>,
+//                    endTime: <string>}
+//OUTPUT: volunteerBlocks: <array> of
+//                         {startTime <string>,
+//                          endTime<string>,
+//                          volunteerCount<string>}
+const getTimeBlocks = (volunteers) => {
+  var times = getUniqueTimes(volunteers)
+  var volunteerCount = toZeroArray(times.length-1)
+  var timeBlocks = []
+
+  volunteers.forEach(volunteer => {
+    var startTime = volunteer.startTime
+    var endTime = volunteer.endTime
+    var startBlockIdx = 0
+    var endBlockIdx = 0
+
+    for(var idx = 0; idx < times.length; idx++)
+      if(startTime === times[idx]){
+        startBlockIdx = idx
+        break;
+      }
+
+    for(var idx = startBlockIdx + 1; idx < times.length; idx++)
+      if(endTime === times[idx]){
+        endBlockIdx = idx
+        break;
+      }
+
+    for(var idx = startBlockIdx; idx < endBlockIdx; idx++){
+      volunteerCount[idx]++
+    }
+  })
+
+  for(var i = 0; i < times.length-1; i++) {
+    timeBlocks[i] = {}
+    timeBlocks[i].startTime = times[i]
+    timeBlocks[i].endTime = times[i+1]
+    timeBlocks[i].volunteerCount = volunteerCount[i]
+  }
+
+  return timeBlocks
+}
+
 
 module.exports = checkEvent
