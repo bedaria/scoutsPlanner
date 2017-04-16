3575dec72118816cec0961d182eb484db7d2d1e1
bedaria <dburinsk@gmail.com>
2017-04-15 23:20:28 -0700

    Plan out layout for app

---
+++ client2/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => (
+  <div className="container">
+  <div className="sidebar">
+    <div className="tabs">
+      <div> Tab 1 </div>
+      <div> Tab 2 </div>
+    </div>
+    <div className="eventButtons">item</div>
+
+
+  </div>
+  <div className="main">
+    <div className="topbar"> TOP BAR </div>
+    <div className="activity"> MAIN </div>
+  </div>
+  </div>
+
+)
+
+export default App
