a0f10f6814b545dcb6f86ec6f5982f2003e13db3
bedaria <dburinsk@gmail.com>
2017-04-15 23:08:53 -0700

    Renames file

---
+++ server/fakeUsers.js
@@ -0,0 +1,43 @@
+const DB = require('./models/index.js')
+const fakeUsers = ["Copper",
+                "Frankie",
+                "Aidan",
+                "Ellen Degeneres",
+                "Beyonce",
+                "Mila Kunis",
+                "Emma Watson",
+                "Jeremy White",
+                "Daniel Kaluuya",
+                "Tom Hardy",
+                "Helen McCrory",
+                "Cillian Murphy",
+                "Joe Cole",
+                "Matt Damon",
+                "Jonah Hill",
+                "Channing Tatum",
+                "Emma Stone",
+                "Emma Roberts",
+                "Jimmy Fallon",
+                "Jimmy Kimmel",
+                "John Oliver",
+                "John Cho",
+                "Kate McKinnon",
+                "Tina Fey",
+                "Samantha Bee",
+                "Michael Che",
+                "Hannah Simone",
+                "Zooey Deschanel",
+                "Lucia",
+                "Alexis Bledel",
+                "Lauren Graham",
+                "Melissa McCarthy",
+                "Keiko Agena",
+                "Sandra Oh",
+                "Kevin McKidd",
+                "Jessee Williams",
+                "Jerrika Hinton"]
+
+fakeUsers.forEach((fakeUser, idx) => setTimeout(() => {
+  console.log("creating: ", fakeUser)
+  DB.User.create({name: fakeUser})
+}, 1000*idx))
