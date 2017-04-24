269d1f91990a64a19493c686a08a2b51e7652c74
bedaria <dburinsk@gmail.com>
2017-04-23 20:11:22 -0700

    EventContainer now works on reload

--- client/containers/EventContainer.js
+++ client/containers/EventContainer.js
@@ -4,26 +4,26 @@ import { Admin } from '../components/Admin'
 import { Invite } from '../components/Invite'
 
 class EventContainer extends Component {
-
-
   render = () => {
-    const { eventsById } = this.props
-    const event = eventsById[this.props.match.params.event]
+    const { eventsById, isFetchingEvent } = this.props
+    let event = Object.keys(eventsById).length ?
+      eventsById[this.props.match.params.event] : {}
 
+    console.log("Object.keys(eventsById).length  ", Object.keys(eventsById).length )
     return (
       <div className="event">
-      {
-        event.isAdmin ? <Admin event={event} /> : <Invite event={event} />
-      }
+        {isFetchingEvent ? <div class="loader" /> : null}
+        {!event.name ? <div> Event does not exist </div> : null}
+        {event.isAdmin ? <Admin event={event} /> : <Invite event={event} /> }
       </div>
     )
   }
 }
 
-const mapStateToProps = ({events: {eventsById }}) => {
+const mapStateToProps = ({events: {eventsById, isFetching }}) => {
   return {
-    eventsById
-  }
+    eventsById,
+    isFetchingEvent: isFetching }
 }
 
 export default connect(
