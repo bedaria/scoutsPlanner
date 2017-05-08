6689af819759a622a3164ee3460c966a6f883ec4
bedaria <dburinsk@gmail.com>
2017-05-08 01:02:56 -0700

    Removes startDateTime, endDateTime attributes

--- server/models/eventVolunteer.js
+++ server/models/eventVolunteer.js
@@ -2,8 +2,6 @@
 
 module.exports = function(sequelize, DataTypes) {
   var EventVolunteer = sequelize.define('EventVolunteer', {
-    startDateTime: DataTypes.DATE,
-    endDateTime: DataTypes.DATE,
     isAttending: {
       type: DataTypes.ENUM,
       values: ['Yes', 'No', 'Maybe']
